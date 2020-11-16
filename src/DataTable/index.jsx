bc70d051a93f968b4bca85001eee39f1cd320680
Jacob Erskov Halland <jeh@novicell.dk>
2020-11-16 18:08:31 +0100

    update comments

--- src/DataTable/index.jsx
+++ src/DataTable/index.jsx
@@ -15,8 +15,8 @@ const rowsInPageNumber = (pageNumber, rowsPerPage) => {
 }
 
 export default function DataTable ({ rows, rowsPerPage = 40 }) {
-  // I decided to only save the data that origins from user input to state, and let the rest be handles as derived state.
-  // Specifically regarding rows data I also opted against saving props.rows in local state, because I preper keeping the props.row as the single source of truth.
+  // I decided to only save the data that origins from user input to state, and let the rest be handled as derived state.
+  // Specifically regarding rows data I also opted against saving props.rows in local state, because I prefer keeping the props.row as the single source of truth.
   const [currentPageNumber, setCurrentPageNumber] = useState(0)
   const [search, setSearch] = useState('')
 
@@ -27,7 +27,7 @@ export default function DataTable ({ rows, rowsPerPage = 40 }) {
 
   // If we have large data sets I would refactor this function using useMemo with dependencies on [rows, search]
   const filteredRows = rows.filter(({ name1, email }) => {
-    // would have liked to use optional chaining ("name?.toLowerCase()") to null check below values
+    // would have liked to use optional chaining ("name?.toLowerCase()") to null check below values - not supported in eslint config
     const nameMatch = name1 && name1.toLowerCase().includes(search.toLowerCase())
     const emailMatch = email && email.toLowerCase().includes(search.toLowerCase())
     return nameMatch || emailMatch
