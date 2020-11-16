97b2f21d3cae8901dacb34755016c0d4407e7e8b
Jacob Erskov Halland <jeh@novicell.dk>
2020-11-16 18:01:08 +0100

    Refactor DataTable and Search to functional component. Move state data to derived state variables and turn Search into controlled input

--- src/DataTable/Search.jsx
+++ src/DataTable/Search.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
-const Search = (props) => {
-  const { onSearch } = props
-
-  return (
-    <div className="p-b-1">
-      <input
-        type="search"
-        className="form-control"
-        placeholder="Søg brugere"
-        onChange={onSearch.bind(this)} />
-    </div>
-  )
-}
+const Search = ({ onSearch, search }) => (
+  // Changed search input form uncontrolled input to controlled input to be able to use it in calculating derived state
+  <div className="p-b-1">
+    <input
+      type="search"
+      className="form-control"
+      placeholder="Søg brugere"
+      value={search}
+      onChange={onSearch} />
+  </div>
+)
 
 export default Search
